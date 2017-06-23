a437f25b600f50c426dc70402f0b3c115215245d
windyakin <windyakin@gmail.com>
2017-06-23 13:52:34 +0900

    .editorconfigを設定する
    Tab => Space2

--- index.js
+++ index.js
@@ -8,35 +8,35 @@ const Slack       = require(__dirname + '/slack.js');
 const EmptyStatus = {'status_emoji': '', 'status_text': ''};
 
 (() => {
-	(function loop(oldStatus) {
-		AppleScript.execFileAsync(__dirname + '/nowplaying.js')
-			.then((str) => {
-				let playing = JSON.parse(str);
-				if ( !(playing.title && playing.artist) ) {
-					return EmptyStatus;
-				}
-				let status = [playing.title, playing.artist].join(' / ');
-				if ( status.length > 100 ) {
-					status = status.slice(0, 97) + '...';
-				}
-				return {'status_emoji': ':headphones:', 'status_text': status};
-			})
-			.catch((err) => {
-				console.error(err);
-			})
-			.then((status) => {
-				// 前のステータスと同じのときはリクエストを送らないようにする
-				if ( JSON.stringify(status) === JSON.stringify(oldStatus) ) {
-					return Promise.reject('Playing song was not changed');
-				}
-				oldStatus = status;
-				return status;
-			})
-			.then((status) => Slack.updateStatusAsync(status))
-			.catch((e) => e)
-			.then((data) => {
-				console.log(moment().format('YYYY-MM-DD HH:mm:ss: ') + data);
-				setTimeout(() => loop(oldStatus), 15000);
-			});
-	})(EmptyStatus);
+  (function loop(oldStatus) {
+    AppleScript.execFileAsync(__dirname + '/nowplaying.js')
+      .then((str) => {
+        let playing = JSON.parse(str);
+        if ( !(playing.title && playing.artist) ) {
+          return EmptyStatus;
+        }
+        let status = [playing.title, playing.artist].join(' / ');
+        if ( status.length > 100 ) {
+          status = status.slice(0, 97) + '...';
+        }
+        return {'status_emoji': ':headphones:', 'status_text': status};
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+      .then((status) => {
+        // 前のステータスと同じのときはリクエストを送らないようにする
+        if ( JSON.stringify(status) === JSON.stringify(oldStatus) ) {
+          return Promise.reject('Playing song was not changed');
+        }
+        oldStatus = status;
+        return status;
+      })
+      .then((status) => Slack.updateStatusAsync(status))
+      .catch((e) => e)
+      .then((data) => {
+        console.log(moment().format('YYYY-MM-DD HH:mm:ss: ') + data);
+        setTimeout(() => loop(oldStatus), 15000);
+      });
+  })(EmptyStatus);
 })();
--- nowplaying.js
+++ nowplaying.js
@@ -4,39 +4,39 @@ const System = Application("System Events");
 var apps = System.processes.name();
 
 var music = {
-	'title': null,
-	'artist': null,
-	'album': null,
+  'title': null,
+  'artist': null,
+  'album': null,
 };
 
 for (var i = 0; i < apps.length; i++) {
-	if (apps[i] === 'iTunes') {
-		const iTunes = Application('iTunes');
-		if (iTunes.playerState() === 'playing') {
-			music['title']  = iTunes.currentTrack.name();
-			music['artist'] = iTunes.currentTrack.artist();
-			music['album']  = iTunes.currentTrack.album();
-			break;
-		}
-	}
-	else if (apps[i] === 'VOX') {
-		const Vox = Application('VOX');
-		if (Vox.playerState() !== 0) {
-			music['title']  = Vox.track();
-			music['artist'] = Vox.artist();
-			music['album']  = Vox.album();
-			break;
-		}
-	}
-	else if (apps[i] === 'Radiant Player') {
-		const RadiantPlayer = Application('Radiant Player');
-		if (RadiantPlayer.playerState() === 2) {
-			music['title']  = RadiantPlayer.currentSongName();
-			music['artist'] = RadiantPlayer.currentSongArtist();
-			music['album']  = RadiantPlayer.currentSongAlbum();
-			break;
-		}
-	}
+  if (apps[i] === 'iTunes') {
+    const iTunes = Application('iTunes');
+    if (iTunes.playerState() === 'playing') {
+      music['title']  = iTunes.currentTrack.name();
+      music['artist'] = iTunes.currentTrack.artist();
+      music['album']  = iTunes.currentTrack.album();
+      break;
+    }
+  }
+  else if (apps[i] === 'VOX') {
+    const Vox = Application('VOX');
+    if (Vox.playerState() !== 0) {
+      music['title']  = Vox.track();
+      music['artist'] = Vox.artist();
+      music['album']  = Vox.album();
+      break;
+    }
+  }
+  else if (apps[i] === 'Radiant Player') {
+    const RadiantPlayer = Application('Radiant Player');
+    if (RadiantPlayer.playerState() === 2) {
+      music['title']  = RadiantPlayer.currentSongName();
+      music['artist'] = RadiantPlayer.currentSongArtist();
+      music['album']  = RadiantPlayer.currentSongAlbum();
+      break;
+    }
+  }
 }
 
 JSON.stringify(music);
