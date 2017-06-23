96adbc3d68af27c0a9146ea2b07647bb4d716032
windyakin <windyakin@gmail.com>
2017-06-23 22:49:41 +0900

    分割

--- src/main.js
+++ src/main.js
@@ -1,37 +1,31 @@
-const Promise = require('bluebird');
+const Bluebird = require('bluebird');
 const Log4js = require('log4js');
-const AppleScript = Promise.promisifyAll(require('applescript'));
+const AppleScript = Bluebird.promisifyAll(require('applescript'));
 
 const logger = Log4js.getLogger();
 const Slack = require('./module/slack.js');
+const TextFormatter = require('./module/text-formatter.js');
 
-const EmptyStatus = { status_emoji: '', status_text: '' };
+global.EmptyStatus = { status_emoji: '', status_text: '' };
 
 require('dotenv').config({ path: './.environment' });
 
 (() => {
   (function loop(oldStatus) {
     let nextStatus = oldStatus;
+
     AppleScript.execFileAsync(`${__dirname}/jxa/nowplaying.js`)
-      .then((str) => {
-        const playing = JSON.parse(str);
-        if (!(playing.title && playing.artist)) {
-          return EmptyStatus;
-        }
-        let status = `${playing.title} / ${playing.artist}`;
-        if (status.length > 100) {
-          status = `${status.slice(0, 97)}...`;
-        }
-        return { status_emoji: ':headphones:', status_text: status };
-      })
+
+      .then(str => TextFormatter.playing2Status(str))
       .catch((err) => {
         logger.error(err);
-        return EmptyStatus;
+        return global.EmptyStatus;
       })
+
       .then((status) => {
         // 前のステータスと同じのときはリクエストを送らないようにする
         if (JSON.stringify(status) === JSON.stringify(oldStatus)) {
-          if (status === EmptyStatus) {
+          if (status === global.EmptyStatus) {
             return Promise.reject('Not playing song now');
           }
           return Promise.reject('Playing song was not changed');
@@ -39,7 +33,9 @@ require('dotenv').config({ path: './.environment' });
         nextStatus = status;
         return status;
       })
+
       .then(status => Slack.updateStatusAsync(status))
+
       .then((data) => {
         const json = JSON.parse(data);
         logger.info(`Status set: ${json.profile.status_text || '(clear)'}`);
@@ -47,8 +43,9 @@ require('dotenv').config({ path: './.environment' });
       .catch((e) => {
         logger.debug(e);
       })
+
       .then(() => {
         setTimeout(() => loop(nextStatus), 10000);
       });
-  }(EmptyStatus));
+  }(global.EmptyStatus));
 })();
---
+++ src/module/text-formatter.js
@@ -0,0 +1,13 @@
+module.exports = class TextFormatter {
+  static playing2Status(json) {
+    const playing = JSON.parse(json);
+    if (!(playing.title && playing.artist)) {
+      return global.EmptyStatus;
+    }
+    let status = `${playing.title} / ${playing.artist}`;
+    if (status.length > 100) {
+      status = `${status.slice(0, 97)}...`;
+    }
+    return { status_emoji: ':headphones:', status_text: status };
+  }
+};
