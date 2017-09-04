c705ad49fe8743adf1d788f5b565d93cd3d4853c
windyakin <windyakin@gmail.com>
2017-09-04 17:30:08 +0900

    stash

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const { ipcMain } = require('electron');
 const menubar = require('menubar');
+const Storage = require('electron-json-storage');
 
 const mb = menubar({
   preloadWindow: true,
+  height: 200,
   index: `file://${__dirname}/view/index.html`,
 });
 
 mb.on('ready', () => {
-  console.log('test');
+  Storage.get('config', (error, data) => {
+    if (error) throw error;
+    if (Object.keys(data).length === 0) {
+      // データがないときの処理
+    } else {
+      // データがあるときの処理
+    }
+  });
 });
 
 ipcMain.on('requsetMessage', (ev, message) => {
-  console.log(message);
+  Storage.set('config', message, (error) => {
+    if (error) throw error;
+  });
   ev.sender.send('responseMessage', 'pong');
 });
